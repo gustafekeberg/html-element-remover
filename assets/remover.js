1f1318c83b4e87cb9a33e4f9341abfa707dd4fcd
Gustaf Ekeberg <firman@gustafekeberg.se>
2017-11-24 13:48:10 +0100

    remove/hide function

--- assets/remover.js
+++ assets/remover.js
@@ -1,33 +1,75 @@
-console.log(config)
+var tagName = `${extensionName}`
+var tagDelete = `${tagName}-remove`
+var tagHide = `${tagName}-hide`
 
-// var strings = ['Godkänd av:', 'Namnförtydligande och befattning:']
-// var id = 'approvalsignature'
+function log(msg) {
+	let formattedMsg = `[${extensionName}] ${JSON.stringify(msg, null, 4)}`
+	console.log(formattedMsg)
+}
 
-// removeSignatureLines()
 
-// function removeSignatureLines() {
-// 	// remove matching strings
-// 	var p = getEls('p');
-// 	for (els of p) {
-// 		removeStrings(els);
-// 	};
-// 	// remove all!? occurring elements of this id
-// 	while (findId(id)) {
-// 		removeEl(findId(id))
-// 	}
-// }
-// function findId(id) {
-// 	return document.getElementById(id)
-// }
-// function getEls(tag) {
-// 	return document.getElementsByTagName(tag)
-// }
-// function removeEl(el) {
-// 	el.parentNode.removeChild(el)
-// }
-// function removeStrings(element) {
-// 	for (string of strings) {
-// 		if (element.innerText == string)
-// 			removeEl(element)
-// 	}
-// }
+function processQuery(config) {
+	log('processQuery')
+	let queries = config.query
+	queries.forEach(item => {
+		let elements = document.querySelectorAll(item.selector)
+		tagElements(elements, item)
+	})
+}
+
+function tagElements(elements, par) {
+	log('tagElements')
+	elements.forEach(element => {
+		if (par.innerHTML == element.innerHTML || par.innerText == element.innerText)
+		addClass(element, par)
+		else if (!par.innerHTML && !par.innerText) {
+			addClass(element, par)
+		}
+	})
+}
+
+function delMarkedElements() {
+	log('delMarkedElements')	
+	function getEl(){
+		return document.querySelector(`.${tagDelete}`);
+	}
+	function delEl(el) {
+		el.parentNode.removeChild(el)
+		log('element removed')
+	}
+	while (getEl())
+	{
+		delEl(getEl())
+	}
+}
+
+function addClass(element, par) {
+	log('addClass')
+	log(par.delete)
+	element.classList.add(tagName)
+	if (par.delete)
+		element.classList.add(tagDelete)
+	else
+		element.classList.add(tagHide)
+}
+
+function addStyleElement() {
+	var CSS =
+	`
+	.${tagName} {
+		opacity: 1
+	}
+	.${tagHide} {
+		opacity: 0;
+		transition: all 500ms
+	}
+	`
+
+	var body = document.querySelector('body')
+	var style = document.createElement('style')
+	style.innerHTML = CSS
+	body.appendChild(style)
+}
+addStyleElement()
+processQuery(config)
+delMarkedElements()
