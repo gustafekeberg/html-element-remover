78d66fb13bdfa0b961607d70361e57ea90734842
Gustaf Ekeberg <firman@gustafekeberg.se>
2017-12-06 23:37:37 +0100

    Adding undo function

--- src/background.js
+++ src/background.js
@@ -1,6 +1,8 @@
 const _browser = require('extensionizer')
 const remover = './assets/remover.js'
-const { logger } = require('./logger')
+const {
+  logger
+} = require('./logger')
 const extensionVarName = _browser.i18n.getMessage("extensionName").split(' ').join('-').toLowerCase()
 const CSSClasses = {
   tagName: extensionVarName,
@@ -11,17 +13,54 @@ const CSSClasses = {
 function createContextMenu(config) {
   logger(`creating context menu items`)
   var menuItems = config.items
-  
-  function prefix(id = '') {
-    let preFixed = `${extensionVarName}_${id}`
-    return preFixed
-  }
+  createMenuItems(menuItems)
+  createMenuItemSeparator()
+  createMenuItemUndo()
 
-  function dePrefix(id) {
-    let len = prefix().length
-    let dePreFixed = id.slice(len, id.length)
-    return dePreFixed
-  }
+  _browser.contextMenus.onClicked.addListener(function (info, tab) {
+    let clickedIndex = dePrefix(info.menuItemId)
+
+    if (clickedIndex !== config.items.length || clickedIndex !== config.items.length - 1) { // Run if not second last or not last
+
+      let clickedConfig = config.items[clickedIndex]
+      logger(`menu item [${clickedConfig.name}] clicked`)
+      addStyle()
+      executeScript(clickedConfig)
+      
+    } else if (clickedConfig == config.items.length) { // Index of separator and undo is at second last and last
+      clickedConfig = 'config that removes changes goes here'
+      executeScript(clickedConfig)
+
+    }
+  })
+}
+
+function executeScript(clickedConfig) {
+  _browser.tabs.executeScript(tab.id, {
+      code: `var config = ${JSON.stringify(clickedConfig)}, extensionName = '${extensionVarName}', CSSClasses = ${JSON.stringify(CSSClasses)}` // make config available to lib
+    },
+    function () {
+      _browser.tabs.executeScript(tab.id, {
+        file: remover
+      })
+    })
+}
+
+function createMenuItemSeparator() {
+  _browser.contextMenus.create({
+    id: prefix("separator"),
+    type: "separator"
+  })
+}
+
+function createMenuItemUndo() {
+  _browser.contextMenus.create({
+    id: prefix("undo"),
+    title: "Undo (remove all hidden elements)"
+  })
+}
+
+function createMenuItems(menuItems) {
   menuItems.forEach((item, index) => {
     let id = prefix(index)
     let name = item.name
@@ -31,21 +70,6 @@ function createContextMenu(config) {
       id: id,
       title: name
     })
-  })
-
-  _browser.contextMenus.onClicked.addListener(function (info, tab) {
-    let clickedIndex = dePrefix(info.menuItemId)
-    let clickedConfig = config.items[clickedIndex]
-    logger(`menu item [${clickedConfig.name}] clicked`)
-    addStyle()
-    _browser.tabs.executeScript(tab.id, {
-      code: `var config = ${JSON.stringify(clickedConfig)}, extensionName = '${extensionVarName}', CSSClasses = ${JSON.stringify(CSSClasses)}` // make config available to lib
-      },
-      function () {
-        _browser.tabs.executeScript(tab.id, {
-          file: remover
-        })
-      })
   })
 }
 
@@ -60,10 +84,22 @@ function addStyle() {
       transition: all 500ms
     }
     `
-  var insertingCSS = _browser.tabs.insertCSS({code: CSS})
+  var insertingCSS = _browser.tabs.insertCSS({
+    code: CSS
+  })
   insertingCSS.then(null, onError())
 }
 
+function prefix(id = '') {
+  let preFixed = `${extensionVarName}_${id}`
+  return preFixed
+}
+
+function dePrefix(id) {
+  let len = prefix().length
+  let dePreFixed = id.slice(len, id.length)
+  return dePreFixed
+}
 
 function onError(error) {
   logger(`error: ${error}`)
@@ -78,7 +114,9 @@ function onGot(item) {
 }
 
 logger(`reading config`)
-var getting = _browser.storage.local.get({json: {}}, function (data, error) {
+var getting = _browser.storage.local.get({
+  json: {}
+}, function (data, error) {
   if (data)
     onGot(data)
   else
